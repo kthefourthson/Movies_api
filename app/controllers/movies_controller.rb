e471d0b9c1f1a115c72a2988da4f0bf7e6c1f2ac
kthefourthson <kthefourthson@gmail.com>
2025-09-18 18:52:33 +1000

    third pass

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,35 @@ class MoviesController < ApplicationController
 
     render json: movies
   end 
+  def show 
+   movies = Movies.find(params[:id])
+
+    render json: movies
+  end 
+  def create 
+    movies = movies.create( 
+    title = movies.title,
+    rating = movies.rating
+    )
+      render json: movies
+  end
+
+  def update 
+    movies = Movie.find(params[:id]) 
+      movies.update( 
+     title: params[:title] || movies.title,
+      rating: params[:rating] || movies.rating,
+      )
+        render json: movies
+    end
+
+    def destroy
+      movies = Movies.find(params[:id])
+  
+      movies.destroy
+  
+      render json: { message: "Movie Deleted..." }
+    end
+
+
 end
