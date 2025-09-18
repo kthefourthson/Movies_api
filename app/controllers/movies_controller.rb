91d9d45a0b947cd461f592d578a336c408e9f255
zunerz671 <zunerz671@gmail.com>
2025-09-18 17:48:22 +0800

    fifth pass

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,37 +3,37 @@ class MoviesController < ApplicationController
     movies = Movie.all
 
     render json: movies
-  end 
-  def show 
+  end
+  def show
    movies = Movies.find(params[:id])
 
     render json: movies
-  end 
-  def create 
+  end
+  def create
     movies = Movie.create(
 
     title: params[:title],
-    rating: params[:rating]
+    rating: params[:rating],
+    year: params[:year]
     )
       render json: movies
   end
 
-  def update 
-    movies = Movie.find(params[:id]) 
-      movies.update( 
-     title: params[:title] || movies.title,
+  def update
+    movies = Movie.find(params[:id])
+      movies.update(
+      title: params[:title] || movies.title,
       rating: params[:rating] || movies.rating,
+      year: params[:year] || movies.year
       )
         render json: movies
     end
 
     def destroy
       movies = Movie.find(params[:id])
-  
+
       movies.destroy
-  
+
       render json: { message: "Movie Deleted..." }
     end
-
-
 end
---
+++ db/migrate/20250918093902_add_year_to_movie.rb
@@ -0,0 +1,5 @@
+class AddYearToMovie < ActiveRecord::Migration[8.0]
+  def change
+    add_column :movies, :year, :integer
+  end
+end
